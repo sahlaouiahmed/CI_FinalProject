de00052e81ea63e2738a8ae610f7708cafc8393d
Ahmed Sahlaoui <sahlaouiahmad@outlook.com>
2024-12-05 22:20:19 +0000

    adding AWS settings to settings.py file

--- kitchen_garden/settings.py
+++ kitchen_garden/settings.py
@@ -185,10 +185,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if 'USE_AWS' in os.environ:
    # Cache control
-   AWS_S3_OBJECT_PARAMETERS = {
-       'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-       'CacheControl': 'max-age=94608000',
-   }
+   #AWS_S3_OBJECT_PARAMETERS = {
+#   'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+#       'CacheControl': 'max-age=94608000',
+#   }
 
    # Bucket Config
    AWS_STORAGE_BUCKET_NAME = 'ci-finalproject'
